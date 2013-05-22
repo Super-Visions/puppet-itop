17a6ac610d5db9a8f0f70e7bc627e699f8b72b5c
Stefan Goethals <stefan@zipkid.eu>
2013-05-22 12:58:42 +0200

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,14 +2,14 @@
 class itop (
   $ensure = undef,
   $install_type = undef,
-  $url = undef,
+  $url = undef
 )
 {
   anchor  { 'itop::start': }->
   class   { 'itop::install': 
     ensure       => $ensure,
     install_type => $install_type,
-    $url         => $url,
+    url          => $url,
   }->
   class   { 'itop::instance': }->
   anchor  { 'itop::end': }

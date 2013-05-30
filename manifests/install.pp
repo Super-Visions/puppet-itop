04e9303b263568b482ccf86db0634eeee19c66b1
Stefan Goethals <stefan@zipkid.eu>
2013-05-30 17:25:34 +0200

    itop instances

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,14 +2,17 @@
 # Class itop::install
 #
 class itop::install (
-  $ensure       = undef,
-  $install_type = 'package',
-  $url          = undef,
+  $ensure         = undef,
+  $install_type   = 'zip',
+  $url            = undef,
+  $extension_hash = undef,
 )
 {
   class { "itop::install::${install_type}":
     ensure => $ensure,
     url    => $url,
   }
-  class { 'itop::extensions': }
+  class { 'itop::extensions':
+    extension_hash => $extension_hash
+  }
 }

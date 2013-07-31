5d07b92f716d6ad722f0add5c613fc84c8c4833a
Stefan Goethals <stefan@zipkid.eu>
2013-07-31 14:45:37 +0200

    anchoring

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,11 +8,13 @@ class itop::install (
   $extension_hash = undef,
 )
 {
+  anchor  { 'itop::install::start': }->
   class { "itop::install::${install_type}":
     ensure => $ensure,
     url    => $url,
-  }
+  }->
   class { 'itop::extensions':
     extension_hash => $extension_hash
-  }
+  }->
+  anchor  { 'itop::install::end': }
 }

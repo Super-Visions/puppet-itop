c72eb67bcfcb2d457eee5f3c457fb9504117b1ec
Stefan Goethals <stefan@zipkid.eu>
2014-02-26 15:57:37 +0100

    test script for itop packaging version checker

---
+++ templates/itop-packaging.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use Data::Dumper;
+use LWP::Simple;
+
+my $url = 'http://localhost:8001/zip';
+
+my $content = get $url;
+die "Couldn't get $url" unless defined $content;
+
+# Then go do things with $content, like this:
+
+my @matches = ( $content =~ m/<a href="(.+)\.zip"/g );
+
+print Dumper \@matches;
+
